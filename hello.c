0519340ca3fb0a94e2aad47bdeeeeabd0d3ff53f
budtsatsral <b26fa1340@efu.edu.mn>
2026-09-19 22:02:17 +0800

    turshilt

--- hello.c
+++ hello.c
@@ -2,8 +2,8 @@
 
 int main(void)
 {
-    printf("Hello, UFE!\n");
-    printf("Hello, My name is Budtsatsral!\n");
-    printf("Hello, UFE!\n");
+    printf("higit, UFE!\n");
+    printf("Hi, My name is Budtsatsral!\n");
+    printf("hi, UFE!\n");
     return 0;
 }
